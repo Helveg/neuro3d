82fc13e4cde223de9bbcf9c3e9f96a5441c1568b
Robin De Schepper <robin.deschepper93@gmail.com>
2021-04-20 15:26:23 +0200

    clone installer branch to test

--- installer/blender_install.py
+++ installer/blender_install.py
@@ -1,5 +1,6 @@
-import bpy
+import bpy, os
 
-bpy.ops.preferences.addon_install(filepath='neuro3d.zip', overwrite=True)
-bpy.ops.preferences.addon_enable(module='neuro3d')
-bpy.ops.wm.save_userpref()
+# bpy.ops.preferences.addon_install(filepath='neuro3d.zip', overwrite=True)
+# bpy.ops.preferences.addon_enable(module='neuro3d')
+# bpy.ops.wm.save_userpref()
+print("Hello there", os.getcwd())
