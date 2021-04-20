c6cb57448bd3d528a316f16c5541f52bab07ccdd
Robin De Schepper <robin.deschepper93@gmail.com>
2021-04-20 16:39:14 +0200

    inlined the install script

--- installer/blender_install.py
+++
@@ -1,7 +0,0 @@
-import bpy, os
-
-bpy.ops.preferences.addon_install(
-    filepath=os.path.abspath("neuro3d.zip"), overwrite=True
-)
-bpy.ops.preferences.addon_enable(module="neuro3d")
-bpy.ops.wm.save_userpref()
