d7d431a767a54d0db3feff8c5f52b34f5fa4b38c
Robin De Schepper <robin.deschepper93@gmail.com>
2021-04-20 15:44:20 +0200

    zip with 'neuro3d' as base dir

--- installer/blender_install.py
+++ installer/blender_install.py
@@ -1,6 +1,5 @@
 import bpy, os
 
-print("Hello there", os.getcwd(), os.path.exists("neuro3d.zip"))
 bpy.ops.preferences.addon_install(filepath=os.path.abspath('neuro3d.zip'), overwrite=True)
-# bpy.ops.preferences.addon_enable(module='neuro3d')
-# bpy.ops.wm.save_userpref()
+bpy.ops.preferences.addon_enable(module='neuro3d')
+bpy.ops.wm.save_userpref()
