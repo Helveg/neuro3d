9a4b336267ee3f3b9236c86e05d709589bb68b87
Robin De Schepper <robin.deschepper93@gmail.com>
2021-04-28 19:57:26 +0200

    can plot any/initial frame

--- neuro3d/_blender/controller.py
+++ neuro3d/_blender/controller.py
@@ -13,10 +13,35 @@ import warnings, functools, pickle, base64, numpy as np
 def _load(obj):
     return pickle.loads(base64.b64decode(obj))
 
+
 def _dump(obj):
-    print("Dumping", obj)
     return base64.b64encode(pickle.dumps(obj)).decode("utf8")
 
+
+def _append_material(obj, color=None):
+    mat = bpy.data.materials.new(obj.name)
+    mat.diffuse_color = color or (1, 1, 1, 1)
+    obj.materials.append(mat)
+
+
+def _create_curve(name, splines=None, color=None, type="CURVE", spline_type=None, **spline_kwargs):
+    curve = bpy.data.curves.new(name, type=type)
+    curve.dimensions = '3D'
+    curve.resolution_u = 2
+    curve.bevel_depth = 0.01
+    _append_material(curve, color)
+    if spline_type is not None:
+        spline_kwargs["type"] = spline_type
+    for data in splines or ():
+        _create_spline(curve, *data, **spline_kwargs)
+    return curve
+
+
+def _create_spline(curve, nurbs, radii, type="POLY"):
+    line = curve.splines.new(type)
+    line.points.add(len(nurbs) - 1)
+    line.points.foreach_set("radius", radii)
+    line.points.foreach_set("co", nurbs.ravel())
 
 class SceneState:
     def __new__(cls, scene=None):
@@ -29,7 +54,6 @@ class SceneState:
         self._scene = scene
         # If `_objects` is already set then we were loaded from a pickle
         if getattr(self, "_objects", False):
-            print("Objs from pickle?")
             self._objects = dict()
             for k,v in self._objects.items():
                 try:
@@ -52,13 +76,12 @@ class SceneState:
 
 
     def _save(self):
-        print("State being saved")
         self._scene.neuro3d.pickle = _dump(self)
 
     def __getstate__(self):
         state = {k: v for k, v in self.__dict__.items() if k not in ("_objects", "_scene")}
         obj_state = {}
-        for k, o in self._objects.items():
+        for k, o in list(self._objects.items()):
             try:
                 o._backend_obj["_n3d_pickle"] = _dump(o)
             except ReferenceError:
@@ -71,7 +94,6 @@ class SceneState:
                     name = "::" + name
                 obj_state[k] = name
         state["_objects"] = obj_state
-        print("Obj stored:", state)
         return state
 
 
@@ -102,11 +124,10 @@ class BlenderController(Controller):
         else:
             self.state._objects[id] = obj
             self.state._next_object_id = max(self.state._next_object_id, id + 1)
+        obj._id = id
         return id
 
-    def create_cell(self, id, cell):
-        self.register_object(cell, id)
-        cell._id = id
+    def create_cell(self, cell):
         cc = self._create_curve_container(cell)
         _init_pos, _init_rot = cell.location, cell.rotation
         cell._backend_obj = cc._backend_obj
@@ -115,35 +136,23 @@ class BlenderController(Controller):
         cell.location = _init_pos
         cell.rotation = _init_rot
 
-    def create_plot(self, id, plot):
-        self.register_object(plot, id)
-        plot._id = id
+    def create_plot(self, plot):
         coll = self._create_collection(plot)
         self.scene.collection.children.link(coll)
-        frame = self._plot_frame(plot)
+        frame = self._create_plot_frame(plot)
         coll.objects.link(frame)
 
     def _create_collection(self, obj):
-        obj._backend_obj = c = bpy.data.collections.new(self.get_name(obj))
+        obj._backend_obj = c = bpy.data.collections.new(self.get_blender_name(obj))
         return c
 
-    def _plot_frame(self, plot):
-        curve = bpy.data.curves.new(self.get_name(plot) + "_frame", type="CURVE")
-        mat = bpy.data.materials.new(self.get_name(plot) + "_frame")
-        mat.diffuse_color = (1, 0, 0, 1)
-        curve.materials.append(mat)
-        curve.dimensions = '3D'
-        curve.resolution_u = 2
-        curve.bevel_depth = 0.01
-        line = curve.splines.new('POLY')
-        line.points.add(2)
-        line.points.foreach_set("radius", [25] * 3)
-        o = np.concatenate((plot._origin, [1]))
-        line.points[0].co = o + [plot._scale[0], 0, 0, 0]
-        line.points[1].co = o
-        line.points[2].co = o + [0, plot._scale[1], 0, 0]
-        obj = bpy.data.objects.new(self.get_name(plot) + "_frame", curve)
-        return obj
+    def _create_plot_frame(self, plot):
+        frame_nurbs = np.column_stack((np.tile(plot._origin, (3, 1)), np.ones(3)))
+        frame_nurbs[0, 1] += plot._scale[1]
+        frame_nurbs[2, 0] += plot._scale[0]
+        name = self.get_blender_name(plot) + "_frame"
+        curve = _create_curve(name, color=(1, 0, 0, 1), splines=((frame_nurbs, [25] * 3),))
+        return bpy.data.objects.new(name, curve)
 
     def get_rotation(self, obj):
         return obj._backend_obj.rotation_euler
@@ -158,14 +167,14 @@ class BlenderController(Controller):
         obj._backend_obj.location = location
 
     def _create_curve_container(self, cell):
-        name = self.get_name(cell)
+        name = self.get_blender_name(cell)
         return CurveContainer(
             cell, _get_curve_template(name), True, _get_default_color(name), 1.0
         )
 
-    def get_name(self, obj):
+    def get_blender_name(self, obj):
         try:
-            return "bn_obj_" + str(obj._id)
+            return "n3d_obj_" + str(obj._id)
         except:
             raise ValueError(f"Could not determine blender object name of '{obj}'.")
 
@@ -178,3 +187,76 @@ class BlenderController(Controller):
         n3d_object = _load(obj["_n3d_pickle"])
         n3d_object._backend_obj = obj
         return n3d_object
+
+    def create_scatter(self, scatter, signal, time):
+        name = self.get_blender_name(scatter._plot) + f"_trace_{len(scatter._plot._traces)}"
+        # Get the coordinates of the scatter on the first frame
+        phases = _sc_frame_phases(scatter, points, times, frame)
+        coords = _sc_frame_to_coords(scatter, signal, time.as_array(copy=False), scatter._plot._window._f0, phases=phases)
+        curve = _create_curve(
+            name,
+            splines=(
+                (
+                    np.column_stack((coords, np.ones(len(coords)))),
+                    [40] * len(coords)
+                ),
+            )
+        )
+        obj = bpy.data.objects.new(name, curve)
+        scatter._plot._backend_obj.objects.link(obj)
+
+
+def _sc_frame_phases(scatter, points, times, fn):
+    f0 = scatter._plot._window._f0
+    t0 = scatter._plot._window._t0
+    st = scatter._plot._image[0]
+    a = scatter._plot._window._a
+    wf = st * 2 * a
+    f = np.vectorize(lambda t: int(round((t - t0) * a) + f0), otypes=[int])
+
+    eb = f(times - st)
+    fases = np.ones(eb.shape, dtype=int) * -1
+    if4 = np.empty(eb.shape, dtype=bool)
+    if0 = np.empty(eb.shape, dtype=bool)
+
+    if4[:-1] = eb[1:] + wf <= fn
+    if4[-1] = if4[-2]
+    fases[if4] = 4
+    if3 = ~if4 & (eb + wf <= fn)
+    fases[if3] = 3
+    if2 = (eb <= fn) & (eb + wf > fn)
+    fases[if2] = 2
+    if0[1:] = eb[:-1] > fn
+    if0[0] = if0[1]
+    fases[if0] = 0
+    if1 = ~if0 & (eb >= fn)
+    fases[if1] = 1
+    return fases
+
+def _sc_frame_to_coords(scatter, points, times, frame, phases=None):
+    if phases is None:
+        phases = _sc_frame_phases(scatter, points, times, frame)
+    coords = np.empty((len(points), 3))
+    coords[:, 2] = scatter._plot._origin[2]
+    ox = scatter._plot._origin[0]
+    sx = scatter._plot._scale[0]
+    st = scatter._plot._image[0]
+    a = scatter._plot._window._a
+    oy = scatter._plot._origin[1]
+    sy = scatter._plot._scale[1]
+    sv = scatter._plot._image[1]
+    f0 = scatter._plot._window._f0
+    t0 = scatter._plot._window._t0
+    f = np.vectorize(lambda t: int(round((t - t0) * a) + f0), otypes=[int])
+    t = np.vectorize(lambda f: t0 + (f + f0) / a, otypes=[float])
+    y = np.vectorize(lambda v: oy + v / sv * sy, otypes=[float])
+    x = np.vectorize(lambda w: ox + sx / 2 + (w - t0) / (2 * st) * sx, otypes=[float])
+    coords[phases < 2, 0] = ox + sx
+    coords[phases > 2, 0] = ox
+    coords[phases == 2, 0] = x(times[phases == 2] - t(frame))
+    coords[phases == 0, 1] = y(points.take(np.nonzero(phases == 0)[0] - 1, mode='clip'))
+    coords[phases == 1, 1] = y(np.interp(t0 + st, times - t(frame), points))
+    coords[phases == 2, 1] = y(points[phases == 2])
+    coords[phases == 3, 1] = y(np.interp(t0 - st, times - t(frame), points))
+    coords[phases == 4, 1] = y(points.take(np.nonzero(phases == 4)[0] + 1, mode='clip'))
+    return coords
--- plotly_test.py
+++ plotly_test.py
@@ -66,7 +66,7 @@ def plot_frames(plot, points, times):
                     dict(
                         label="Play",
                         method="animate",
-                        args=[None, {"fromcurrent": True}]
+                        args=[None, {"frame": {"duration": 200}, "transtion": {"duration": 0}}]
                     ),
                     dict(
                         label="Pause",
@@ -81,56 +81,117 @@ def plot_frames(plot, points, times):
 
 
 def animate_frames(plot, points, times):
+    points = np.array(points)
+    times = np.array(times)
     ox = plot._origin[0]
     sx = plot._scale[0]
     ft = plot._window._ft
-    a = plot._window._a
-    wf = sx * 2 * a
+    fn = plot._window._fn
     st = plot._image[0]
+    a = plot._window._a
+    wf = st * 2 * a
+    print(wf)
     oy = plot._origin[1]
     sy = plot._scale[1]
+    sv = plot._image[1]
     f0 = plot._window._f0
     t0 = plot._window._t0
-    print(a)
-    def f(t): return int(round((t - t0) * a) + f0)
-    fx1 = np.array([f(t - st) for t in times])
-    fx2 = np.array([f(t + st) for t in times])
-    fy1 = [0] + fx1[:-1]
-    fy2 = fx1.copy()
-    fy3 = fx2.copy()
-    fy4 = fx2[1:] + [0]
-    print("xtr1:", fx1)
-    print("xtr2:", fx2)
-    print("ytr1:", fy1)
-    print("ytr2:", fy2)
-    print("ytr3:", fy3)
-    print("ytr4:", fy4)
-    init_pre = np.nonzero(fx1 > f0)[0]
-    init_post = np.nonzero(fx1 + wf < f0)[0]
-    init_post_pre = np.nonzero(fx1 > f0)[0]
-    init_in = np.nonzero(fx1 > f0)[0]
-    init_pre_post = np.nonzero(fx1 + wf < f0)[0]
-    print("initial pre-ytr1", init_pre)
-    print("initial post-ytr4", init_post)
-    exit()
-    def fy(y): return 0
-    f = go.Frame(
-        data=[
-            go.Scatter(x=[ox, ox, ox + sx], y=[oy + sy, oy, oy], name="frame"),
-            go.Scatter(x=[ox + sx / 2] * 2, y=[oy, oy + sy], name="t"),
-            go.Scatter(x=[fx(x) for x in times], y=[fy(y) for y in points], name="curves"),
-        ],
-        layout=go.Layout(
-            xaxis=dict(
-                tickmode = "array",
-                tickvals = [ox, ox + sx / 2, ox + sx],
-                ticktext = [round(t - st, 5), round(t, 5), round(tmm, 5)],
-            ),
-            title=f"Sliding plot #{frame} ({round(t, 4)})",
+    rbx = ox + sx
+    f = np.vectorize(lambda t: int(round((t - t0) * a) + f0), otypes=[int])
+    t = np.vectorize(lambda f: t0 + (f + f0) / a, otypes=[float])
+    y = np.vectorize(lambda v: oy + v / sv * sy, otypes=[float])
+    x = np.vectorize(lambda w: ox + sx / 2 + (w - t0) / (2 * st) * sx, otypes=[float])
+    eb = np.fromiter((f(t - st) for t in times), dtype=int)
+    # print("event base:", eb)
+    # visibility = [[0, 'rgba(255, 0, 0, 1)'], [1.0, 'rgba(255, 0, 0, 0)']]
+    def animate_frame(fn):
+        frame_x = np.empty(eb.shape)
+        frame_y = np.empty(eb.shape)
+        fases = np.ones(eb.shape) * -1
+        if4 = np.empty(eb.shape, dtype=bool)
+        if0 = np.empty(eb.shape, dtype=bool)
+        # Mask of points initially in fase 4 (located to the left of window)
+        if4[:-1] = eb[1:] + wf < fn
+        # Endpoint has no y4 transition, use previous point y4 transition
+        if4[-1] = if4[-2]
+        fases[if4] = 4
+        if3 = ~if4 & (eb + wf < fn)
+        fases[if3] = 3
+        # print("fase 3 or 4", eb + wf <= fn)
+        if0[1:] = eb[:-1] > fn
+        # Startpoint has no y1 transition, use next point y1 transition
+        if0[0] = if0[1]
+        fases[if0] = 0
+        if1 = ~if0 & (eb >= fn)
+        fases[if1] = 1
+        if2 = (eb < fn) & (eb + wf >= fn)
+        fases[if2] = 2
+        frame_x[fases < 2] = ox + sx
+        frame_x[fases > 2] = ox
+        frame_x[fases == 2] = x(times[fases == 2] - t(fn))
+        # print("zero phase waiting points:", np.nonzero(fases == 0)[0] - 1, points, points.take(np.nonzero(fases == 0)[0] - 1, mode='clip'))
+        frame_y[fases == 0] = y(points.take(np.nonzero(fases == 0)[0] - 1, mode='clip'))
+        frame_y[fases == 1] = y(np.interp(t0 + st, times - t(fn), points))
+        frame_y[fases == 2] = y(points[fases == 2])
+        frame_y[fases == 3] = y(np.interp(t0 - st, times - t(fn), points))
+        frame_y[fases == 4] = y(points.take(np.nonzero(fases == 4)[0] + 1, mode='clip'))
+        # print(" ------ Frame report:", f0)
+        # print("fases", fases)
+        # print(frame_x)
+        # print(frame_y)
+        # print(points, y(points))
+        # exit()
+        return frame_x, frame_y
+
+    def get_initial_phases(points, times):
+        f0 = plot._window._f0
+        st = plot._image[0]
+        a = plot._window._a
+        wf = st * 2 * a
+        f = np.vectorize(lambda t: int(round((t - t0) * a) + f0), otypes=[int])
+
+        eb = f(times - st)
+        fases = np.ones(eb.shape) * -1
+        if4 = np.empty(eb.shape, dtype=bool)
+        if0 = np.empty(eb.shape, dtype=bool)
+
+        if4[:-1] = eb[1:] + wf <= f0
+        if4[-1] = if4[-2]
+        fases[if4] = 4
+        if3 = ~if4 & (eb + wf <= f0)
+        fases[if3] = 3
+        if2 = (eb <= f0) & (eb + wf > f0)
+        fases[if2] = 2
+        if0[1:] = eb[:-1] > f0
+        if0[0] = if0[1]
+        fases[if0] = 0
+        if1 = ~if0 & (eb >= f0)
+        fases[if1] = 1
+        return fases
+
+    print(get_initial_phases(points, times))
+    frame_data = [animate_frame(f) for f in range(f0, fn + 1)]
+    frames = [
+        go.Frame(
+            data=[
+                go.Scatter(x=[ox, ox, ox + sx], y=[oy + sy, oy, oy], name="frame"),
+                go.Scatter(x=[ox + sx / 2] * 2, y=[oy, oy + sy], name="t"),
+                go.Scatter(x=frame_x, y=frame_y, name="frame_curve"),
+                go.Scatter(x=x(times - t(fn)), y=y(points), name="full curve"),
+            ],
+            layout=go.Layout(
+                # xaxis=dict(
+                #     tickmode = "array",
+                #     tickvals = [ox, ox + sx / 2, ox + sx],
+                #     ticktext = [round(t(fn) - st, 5), round(t(f0 + fn), 5), round(t(fn) + st, 5)],
+                # ),
+                title=f"Sliding plot #{fn} ({round(float(t(fn)), 4)})",
+            )
         )
-    )
-    return f
+        for fn, (frame_x, frame_y) in enumerate(frame_data)
+    ]
+    return frames
 
 # origin, scale, image_scale, frame_window
-plot = Plot([0, 0, 0], [100, 100, 0], [1, 100], FrameWindow(0, 100, 0, 1))
-plot_frames(plot, [0, 10], [1.1, 1.12, 1.18, 1.2])
+plot = Plot([10, 0, 0], [100, 100, 0], [1, 50], FrameWindow(0, 100, 0, 2))
+plot_frames(plot, [1, 10, 2, 5, 15, 3], [-2, -1.01, -0.0, 1.1, 1.18, 1.2])
